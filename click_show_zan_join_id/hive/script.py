690e21adac0fa4bc84da015fa5a7865b5fc21c97
yinwoods <yinwoods@163.com>
2017-02-08 05:49:09 +0000

    update

--- click_show_zan_join_id/hive/script.py
+++ click_show_zan_join_id/hive/script.py
@@ -1,25 +1,26 @@
 import os
 import time
+import arrow
 import subprocess
-from datetime import datetime,timedelta
 
 def fileExist(latestTime):
-    print(latestTime)
-    command = 'hdfs dfs -test -e wasb://niphdid@nipspark.blob.core.windows.net/user/zhangrn/click_show_join/{date}/2300/_SUCCESS'.format(date=date)
+    date = latestTime.format('YYYYMMDD')
+    hour = latestTime.format('HH')
+    command = 'hdfs dfs -test -e wasb://niphdid@nipspark.blob.core.windows.net/user/zhangrn/click_show_join/{date}/{hour}00/_SUCCESS'.format(date=date, hour=hour)
     print(command)
     if subprocess.call(command, shell=True):
         print('file does not exist yet, wait 1 hour.')
         time.sleep(3600)
     else:
         print('start executing hive')
-        command = 'hive -hiveconf date={date} -hiveconf hour={hour} -f getData.sql'.format(date=date, hour=hour)
+        print('get {date}/{hour}'.format(date=date, hour=hour))
+        command = 'hive -hiveconf date={date} -hiveconf hour={hour}00 -f getData.sql'.format(date=date, hour=hour)
         p = subprocess.Popen(command, shell=True)
         p.wait()
 
 
 def getLatestHour():
     destpos = os.path.join(os.getcwd(), 'data')
-    print(os.listdir(destpos))
     date = max(os.listdir(destpos))
     path = os.path.join(destpos, date)
     hour = max(os.listdir(path))
@@ -27,14 +28,11 @@ def getLatestHour():
     month = int(date[4:6])
     day = int(date[6:8])
     hour = int(hour[:2])
-    latestTime = datetime(year, month, day, hour)
-    print(latestTime)
-    now = latestTime + timedelta(hours=1)
+    latestTime = arrow.get(year, month, day, hour)
+    now = latestTime.replace(hours=+1)
     return now
 
 
 while True:
-    now = getLatestHour()
-    latestTime = now.strftime('%Y%m%d')
-    print(latestTime)
+    latestTime = getLatestHour()
     fileExist(latestTime)

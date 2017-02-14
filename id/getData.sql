9218a04397d128db0f14dc03c9538d08f7cd99c4
yinwoods <yinwoods@163.com>
2017-02-14 05:30:44 +0000

    add run.py

--- id/getData.sql
+++ id/getData.sql
@@ -37,7 +37,7 @@ create external table if not exists impression
 partitioned by (d string, h string)
 row format delimited fields terminated by '\t';
 
-alter table impression add partition(d='${hiveconf:date}', h='${hiveconf:hour_current}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dailyCtr/NewsImpressionSummary-${hiveconf:date}/${hiveconf:hour_current}';
+alter table impression add partition(d='${hiveconf:date}', h='${hiveconf:hour_left4}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dailyCtr/NewsImpressionSummary-${hiveconf:date}/${hiveconf:hour_left4}';
 
 
 drop table if exists click;
@@ -72,13 +72,11 @@ create external table if not exists click
 partitioned by (d string, h string)
 row format delimited fields terminated by '\t';
 
+alter table click add partition(d='${hiveconf:date}', h='${hiveconf:hour_left5}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dailyCtr/NewsClickSummary-${hiveconf:date}/${hiveconf:hour_left5}';
+alter table click add partition(d='${hiveconf:date}', h='${hiveconf:hour_left4}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dailyCtr/NewsClickSummary-${hiveconf:date}/${hiveconf:hour_left4}';
+alter table click add partition(d='${hiveconf:date}', h='${hiveconf:hour_left3}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dailyCtr/NewsClickSummary-${hiveconf:date}/${hiveconf:hour_left3}';
+alter table click add partition(d='${hiveconf:date}', h='${hiveconf:hour_left2}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dailyCtr/NewsClickSummary-${hiveconf:date}/${hiveconf:hour_left2}';
 alter table click add partition(d='${hiveconf:date}', h='${hiveconf:hour_left1}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dailyCtr/NewsClickSummary-${hiveconf:date}/${hiveconf:hour_left1}';
-alter table click add partition(d='${hiveconf:date}', h='${hiveconf:hour_current}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dailyCtr/NewsClickSummary-${hiveconf:date}/${hiveconf:hour_current}';
-alter table click add partition(d='${hiveconf:date}', h='${hiveconf:hour_right1}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dailyCtr/NewsClickSummary-${hiveconf:date}/${hiveconf:hour_right1}';
-alter table click add partition(d='${hiveconf:date}', h='${hiveconf:hour_right2}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dailyCtr/NewsClickSummary-${hiveconf:date}/${hiveconf:hour_right2}';
-alter table click add partition(d='${hiveconf:date}', h='${hiveconf:hour_right3}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dailyCtr/NewsClickSummary-${hiveconf:date}/${hiveconf:hour_right3}';
-
-
 
 
 drop table if exists dianzan;
@@ -92,29 +90,15 @@ create external table if not exists dianzan
 partitioned by (d string, h string)
 row format delimited fields terminated by '\t';
 
+alter table dianzan add partition(d='${hiveconf:date}', h='${hiveconf:hour_left12}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dashboard_offline_dt/${hiveconf:date}/${hiveconf:hour_left12}00';
+alter table dianzan add partition(d='${hiveconf:date}', h='${hiveconf:hour_left11}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dashboard_offline_dt/${hiveconf:date}/${hiveconf:hour_left11}00';
+alter table dianzan add partition(d='${hiveconf:date}', h='${hiveconf:hour_left10}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dashboard_offline_dt/${hiveconf:date}/${hiveconf:hour_left10}00';
+alter table dianzan add partition(d='${hiveconf:date}', h='${hiveconf:hour_left9}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dashboard_offline_dt/${hiveconf:date}/${hiveconf:hour_left9}00';
 alter table dianzan add partition(d='${hiveconf:date}', h='${hiveconf:hour_left8}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dashboard_offline_dt/${hiveconf:date}/${hiveconf:hour_left8}00';
-alter table dianzan add partition(d='${hiveconf:date}', h='${hiveconf:hour_left7}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dashboard_offline_dt/${hiveconf:date}/${hiveconf:hour_left7}00';
-alter table dianzan add partition(d='${hiveconf:date}', h='${hiveconf:hour_left6}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dashboard_offline_dt/${hiveconf:date}/${hiveconf:hour_left6}00';
-alter table dianzan add partition(d='${hiveconf:date}', h='${hiveconf:hour_left5}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dashboard_offline_dt/${hiveconf:date}/${hiveconf:hour_left5}00';
-alter table dianzan add partition(d='${hiveconf:date}', h='${hiveconf:hour_left4}') location 'wasb://niphdid@nipspark.blob.core.windows.net/dashboard_offline_dt/${hiveconf:date}/${hiveconf:hour_left4}00';
 
 
 drop table if exists click_show_join;
-create table click_show_join
-(
-    pageindex int,
-    tag string,
-    newstype string,
-    mediaid int,
-    categoryid int,
-    requestcategoryid int,
-    index int,
-    vote_up int,
-    voite_down int
-)
-row format delimited fields terminated by '\t';
-
-INSERT OVERWRITE TABLE click_show_join
+create temporary table if not exists click_show_join as
 select
     impression.pageindex,
     impression.tag,
@@ -122,6 +106,7 @@ select
     impression.mediaid,
     impression.categoryid,
     impression.requestcategoryid,
+    IF(click.newsid is NULL, 0, 1) as clicked,
     click.index,
     dianzan.vote_up,
     dianzan.vote_down
@@ -144,4 +129,4 @@ dianzan
     and impression.newsid = dianzan.newsid
 );
 
-INSERT OVERWRITE DIRECTORY 'wasb://niphdid@nipspark.blob.core.windows.net/user/zhangrn/click_show_join/${hiveconf:date}/${hiveconf:hour_current}00' ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' SELECT * from click_show_join;
+INSERT OVERWRITE DIRECTORY 'wasb://niphdid@nipspark.blob.core.windows.net/user/zhangrn/click_show_join/${hiveconf:date}/${hiveconf:hour_left4}' ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' SELECT * from click_show_join;
--- id/run.sh
+++ id/run.sh
@@ -1,13 +1,27 @@
+time=`da`
+date=`date +%Y%m%d`
+echo $date
+hour_left1=`date -d -1hour +%H`
+hour_left2=`date -d -2hour +%H`
+hour_left3=`date -d -3hour +%H`
+hour_left4=`date -d -4hour +%H`
+hour_left8=`date -d -8hour +%H`
+hour_left9=`date -d -9hour +%H`
+hour_left10=`date -d -10hour +%H`
+hour_left11=`date -d -11hour +%H`
+hour_left12=`date -d -12hour +%H`
+
+echo $date, $hour_left1, $hour_left12
+
 hive\
-    -hiveconf date=20170212\
-    -hiveconf hour_current=15\
-    -hiveconf hour_left1=14\
-    -hiveconf hour_left4=11\
-    -hiveconf hour_left5=10\
-    -hiveconf hour_left6=09\
-    -hiveconf hour_left7=08\
-    -hiveconf hour_left8=07\
-    -hiveconf hour_right1=16\
-    -hiveconf hour_right2=17\
-    -hiveconf hour_right3=18\
+    -hiveconf date=$date\
+    -hiveconf hour_left1=$hour_left1\
+    -hiveconf hour_left2=$hour_left2\
+    -hiveconf hour_left3=$hour_left3\
+    -hiveconf hour_left4=$hour_left4\
+    -hiveconf hour_left8=$hour_left8\
+    -hiveconf hour_left9=$hour_left9\
+    -hiveconf hour_left10=$hour_left10\
+    -hiveconf hour_left11=$hour_left11\
+    -hiveconf hour_left12=$hour_left12\
     -f getData.sql
